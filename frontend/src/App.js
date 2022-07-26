14c4152a4b5383db211444f4e4e1682c8058aae8
Ankush263 <ankushbanik263@gmail.com>
2022-07-26 11:03:22 +0530

    Add connect to metamask functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { ethers } from 'ethers';
+import ABI from './artifacts/contracts/ERC20Token.sol/ERC20Token.json'
 
 function App() {
+
+  const [logedIn, setLogedIn] = useState(false)
+  const [amount, setAmount] = useState(0)
+  const [address, setAddress] = useState("")
+
+  const contractAddress = address
+  const abi = ABI.abi
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
+
+  const connect = async () => {
+
+    try{
+      if (typeof window.ethereum !== 'undefined') {
+        await window.ethereum.request({ method: 'eth_requestAccounts' })
+        setLogedIn(true)
+      }
+    }catch(error){
+      console.log(error)
+    }
+
+  }
+
+  const sendToken = async () => {
+
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!logedIn && <button onClick={connect}>Connect Wallet</button>}
+      <div className="send--token">
+        <input type="text" placeholder="Enter The Reciver Address" onChange={(e) => setAddress(e.target.value)} value={address} />
+        <input type="number" placeholder="Enter value" onChange={(e) => setAmount(e.target.value)} value={amount} />
+        <button onClick={sendToken}>Send Token</button>
+      </div>
     </div>
   );
 }
