41980ab6991bd5ca082ef094900005c2eb8d24fa
Ankush263 <ankushbanik263@gmail.com>
2022-07-26 22:47:58 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ethers } from 'ethers';
 import ABI from './artifacts/contracts/ERC20Token.sol/ERC20Token.json'
 
@@ -8,38 +8,82 @@ function App() {
   const [logedIn, setLogedIn] = useState(false)
   const [amount, setAmount] = useState(0)
   const [address, setAddress] = useState("")
+  const [tokenName, setTokenName] = useState("")
+  const [tokenSymbol, setTokenSymbol] = useState("")
+  const [sendAddress, setSendAddress] = useState("")
+  const [tokenAmount, setTokenAmount] = useState(0)
 
-  const contractAddress = address
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
   const abi = ABI.abi
   const provider = new ethers.providers.Web3Provider(window.ethereum)
   const signer = provider.getSigner()
+  // setAddress(contractAddress)
+  
 
   const connect = async () => {
-
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+    // setAddress(await signer.getAddress())
     try{
       if (typeof window.ethereum !== 'undefined') {
         await window.ethereum.request({ method: 'eth_requestAccounts' })
+        const Address = await signer.getAddress()
+        const Amount = await contract.balanceOf(Address)
+        const Name = await contract.name()
+        const Symbol = await contract.symbol()
         setLogedIn(true)
+        setAddress(Address)
+        setAmount(Amount.toNumber())
+        setTokenName(Name)
+        setTokenSymbol(Symbol)
+        // console.log(Amount.toNumber())
       }
     }catch(error){
+      console.log("Error: ", error)
+    }
+
+  }
+
+
+  const send = async () => {
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+
+    try {
+      await contract.transfer(
+        sendAddress,
+        tokenAmount
+      )
+    } catch (error) {
       console.log(error)
     }
 
   }
 
-  const sendToken = async () => {
-
+  const show = async () => {
     const contract = new ethers.Contract(contractAddress, abi, signer)
 
+    try {
+      const Show = await contract.balanceOf(sendAddress)
+      console.log(Show.toNumber())
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <div className="App">
-      {!logedIn && <button onClick={connect}>Connect Wallet</button>}
-      <div className="send--token">
-        <input type="text" placeholder="Enter The Reciver Address" onChange={(e) => setAddress(e.target.value)} value={address} />
-        <input type="number" placeholder="Enter value" onChange={(e) => setAmount(e.target.value)} value={amount} />
-        <button onClick={sendToken}>Send Token</button>
+      {!logedIn ? <button onClick={connect}>Connect Wallet</button> : <div>Connected</div>}
+      <div>
+        <h3>Account Adderss : {address}</h3>
+        <h4>Name: {tokenName}  Symbol: {tokenSymbol}  Amount : {amount}</h4>
+      </div>
+      <div>
+        <input type="text" placeholder='Enter the recipent Address' onChange={(e) => setSendAddress(e.target.value)} value={sendAddress} />
+        <input type="number" placeholder='Enter the amount of tokens' onChange={(e) => setTokenAmount(e.target.value)} value={tokenAmount} />
+        <button onClick={send}>Send</button>
+      </div>
+      <div>
+        <input type="text" placeholder='Enter the recipent Address' onChange={(e) => setSendAddress(e.target.value)} />
+        <button onClick={show}>Show</button>
       </div>
     </div>
   );
