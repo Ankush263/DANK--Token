3d38b3a628361abb8a57a797586f6b9558e70d93
Ankush263 <ankushbanik263@gmail.com>
2022-07-30 11:07:08 +0530

    Update contract

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract DANKToken is ERC20 {
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 100000 * (10 ** 18));
+        _mint(msg.sender, 100000 * (10 ** 2));
     }
 
     function faucet (address recipient , uint amount) external {
