a4f7ff00d7f7c5f3126b90809573f95f5cd5bedc
Ankush263 <ankushbanik263@gmail.com>
2022-07-30 17:42:03 +0530

    Update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,61 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-ethers");
+// require("@nomicfoundation/hardhat-toolbox");
+// require("@nomiclabs/hardhat-ethers");
+// require("@nomiclabs/hardhat-waffle");
+// require("dotenv").config();
+
+// const GOERLI_URL = process.env.GOERLI_URL
+// const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.9",
+//   paths: {
+//     artifacts: './src/artifacts',
+//   },
+//   networks: {
+//     goerli: {
+//       url: GOERLI_URL,
+//       accounts: [PRIVATE_KEY]
+//     }
+//   }
+// };
+
+
+
+
 require("@nomiclabs/hardhat-waffle");
-require("dotenv").config();
+require('dotenv').config()
 
-const GOERLI_URL = process.env.GOERLI_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
 
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.9",
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
   paths: {
-    artifacts: './frontend/src/artifacts',
+    artifacts: './src/artifacts',
   },
   networks: {
-    goerli: {
-      url: GOERLI_URL,
-      accounts: [PRIVATE_KEY]
+    hardhat: {
+      chainId: 1337
+    },
+    goreli: {
+      url: `https://goerli.infura.io/v3/${process.env.API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+
     }
-  }
+  },
+  solidity: "0.8.3"
 };
