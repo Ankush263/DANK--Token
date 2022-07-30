ffb868ce74a5ef21733544fba1eeaa683dd90049
Ankush263 <ankushbanik263@gmail.com>
2022-07-30 17:42:18 +0530

    make payable(optional)

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -7,10 +7,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract DANKToken is ERC20 {
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 100000 * (10 ** 2));
+        _mint(msg.sender, 100000 * (10 ** 18));
     }
 
-    function faucet (address recipient , uint amount) external {
+    function faucet (address payable recipient , uint amount) payable external {
       _mint(recipient, amount);
     }
 }
